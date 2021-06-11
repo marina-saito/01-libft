83db9075c29304c8bea5c895c0b89e61b32fca60
Marina <marina.ssaito@gmail.com>
2021-06-11 20:50:07 -0300

    Checking case when dst or src is NULL

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: msayuri- <msayuri-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 07:06:33 by msayuri-          #+#    #+#             */
-/*   Updated: 2021/06/06 18:03:40 by msayuri-         ###   ########.fr       */
+/*   Updated: 2021/06/11 20:49:33 by msayuri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	i = 0;
 	s = (char *)dst;
+	if (src == NULL || dst == NULL)
+		return ((void *)s);
 	while (i < n)
 	{
 		*(s + i) = *((char *)src + i);
