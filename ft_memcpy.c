69a642cf64194b5c079b5c173ed36fea5b085098
Marina <marina.ssaito@gmail.com>
2021-06-12 09:38:57 -0300

    Trying to fix memcpy - 7

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: msayuri- <msayuri-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 07:06:33 by msayuri-          #+#    #+#             */
-/*   Updated: 2021/06/12 09:30:14 by msayuri-         ###   ########.fr       */
+/*   Updated: 2021/06/12 09:38:46 by msayuri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
-	char	*dst_temp;
-	char	*src_temp;
+	size_t			i;
+	unsigned char	*dst_temp;
+	unsigned char	*src_temp;
 
 	i = 0;
-	dst_temp = (char *)dst;
-	src_temp = (char *)src;
+	dst_temp = (unsigned char *)dst;
+	src_temp = (unsigned char *)src;
 	while (i < n)
 	{
 		dst_temp[i] = src_temp[i];
