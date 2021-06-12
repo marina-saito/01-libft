da39a0f92d114c0151d4fb8913071c4ac7c868ec
Marina <marina.ssaito@gmail.com>
2021-06-11 21:30:10 -0300

    Trying to fix memccpy and strlcpy - 2nd try

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: msayuri- <msayuri-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 12:39:10 by msayuri-          #+#    #+#             */
-/*   Updated: 2021/06/11 21:26:15 by msayuri-         ###   ########.fr       */
+/*   Updated: 2021/06/11 21:30:05 by msayuri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,16 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	src_len;
-	char	*src_cpy;
 
-	src_cpy = src;
-	src_len = ft_strlen(src_cpy);
+	src_len = ft_strlen(src);
 	i = 0;
 	if (dstsize == 0)
 		return (src_len);
 	while (i < dstsize - 1)
 	{
-		if (*(src_cpy + i) == '\0')
+		if (*(src + i) == '\0')
 			break ;
-		*(dest + i) = *(src_cpy + i);
+		*(dest + i) = *(src + i);
 		i++;
 	}
 	*(dest + i) = '\0';
