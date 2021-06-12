dc119fbf3788e58527f0c8abfaa2afeaf8c76c40
Marina <marina.ssaito@gmail.com>
2021-06-11 21:01:02 -0300

    Checking case when dst or src is NULL - forth try

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: msayuri- <msayuri-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 07:06:33 by msayuri-          #+#    #+#             */
-/*   Updated: 2021/06/11 20:53:57 by msayuri-         ###   ########.fr       */
+/*   Updated: 2021/06/11 20:59:35 by msayuri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	i = 0;
 	s = (char *)dst;
 	if (src == NULL || dst == NULL)
-		return (dst);
+		write(1,"loucura!", 8);
 	while (i < n)
 	{
 		*(s + i) = *((char *)src + i);
